f0b2af85c0c9893ce85178a5cd4a42b478ab523e
Shoaib Burq <saburq@gmail.com>
2011-05-11 19:03:53 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrowingMist252::Application.config.secret_token = '3136f95935ff0fa38f87455ae662488af3848dc29746f0ffab8a6abad793ae694a4e1b1c70b2871d49a111f8d7d85467ec989bd787409466d32ec3e551ccf93f'
